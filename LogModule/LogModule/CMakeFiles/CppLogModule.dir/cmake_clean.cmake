ce2c5755f7dea79c73bc2227cba123db3cfe65a6
=Northell <northells@yandex.ru>
2024-01-30 17:17:26 +0300

    Updated

---
+++ LogModule/LogModule/CMakeFiles/CppLogModule.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppLogModule.dir/LogAPI.cpp.o"
+  "CMakeFiles/CppLogModule.dir/src/LogModule.cpp.o"
+  "libCppLogModule.a"
+  "libCppLogModule.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppLogModule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
