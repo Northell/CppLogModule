c3110c38e57c902ffa6ccd8676cbf5d05b4ce9d8
Nrt <northells@yandex.ru>
2024-01-30 13:46:21 +0300

    Updated

--- LogModule/LogModule/LogAPI.h
+++ LogModule/LogModule/LogAPI.h
@@ -7,7 +7,13 @@ namespace LogModule
 	extern  __declspec(dllexport) void write_log(const wchar_t* pathToFile, const wchar_t* classInvoker, const wchar_t* methodInvoker, const wchar_t* message);
 	extern  __declspec(dllexport) void write_log(const std::string& logFileName, const std::string& classInvoker, const std::string& methodInvoker, const std::string& message);
 	extern  __declspec(dllexport) void set_debug(bool isDebug);
+#else
+	extern void write_log(const wchar_t* pathToFile, const wchar_t* classInvoker, const wchar_t* methodInvoker, const wchar_t* message);
+	extern void write_log(const std::string& logFileName, const std::string& classInvoker, const std::string& methodInvoker, const std::string& message);
+	extern void set_debug(bool isDebug);
+
 #endif // _WIN32
+
 
 }
 
